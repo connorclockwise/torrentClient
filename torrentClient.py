cba9a2bf77cb28a46fa66e5584f47463d55bc09a
Connor <connor@connor-System.(none)>
2014-04-13 15:55:54 -0400

    We now have a peer list from the server

--- torrentClient.py
+++ torrentClient.py
@@ -14,60 +14,9 @@ def main(args):
 	if len(args) != 3:
 		sys.exit("\nInvalid syntax, please use the arguments 'arg1: source, arg2: destination")
 
+	peerList = getPeerList(args[1])
+	print peerList
 	
-	decodedDict= bencode.bdecode(open(args[1], 'rb').read())
-
-	torrentTrackerUrl = decodedDict["announce"]
-	slash = torrentTrackerUrl.find("/")
-	torrentTrackerHostName = torrentTrackerUrl[slash + 2:]
-	slash2 = torrentTrackerHostName.find("/")
-	torrentTrackerHostName = torrentTrackerHostName[:slash2]
-	colon = torrentTrackerHostName.find(":")
-	torrentTrackerHostName = torrentTrackerHostName[:colon]
-	# print torrentTrackerUrl
-	# print torrentTrackerHostName
-
-
-
-	sha1 = hashlib.sha1()
-	bencodedInfo = bencode.bencode(decodedDict["info"])
-	sha1.update(bencodedInfo)
-	hashed_info = sha1.digest()
-	info_hash = urllib.quote(hashed_info)
-	# hexstring = hashed_info.encode("hex")
-	# info_hash = ""
-	# # print hexstring
-	# for count in range(0, len(hexstring),2):
-	# 	info_hash =  info_hash + "%" + hexstring[count:count+2]
-	peer_id = "abcdefghijklmonpqrst"
-	# peer_id = urllib.quote(peer_id)
-	
-	uploaded = 0
-	downloaded = 0 
-	left = decodedDict["info"]["length"]
-	compact = 0
-	no_peer_id = 0
-	event = "started"
-
-	# print(str(to"rrentTrackerHostName))
-	# print decodedDict["announce-list"]
-
-	getRequest = ( torrentTrackerUrl +
-	"?info_hash=" + info_hash  +
-	"&peer_id=" + peer_id  +
-	"&port=6881" +
-	"&uploaded=" + str(uploaded)  +
-	"&downloaded=" + str(downloaded)  +
-	"&left=" + str(left) +
-	"&compact=" + str(1) +
-	"&no_peer_id=0" +
-	"&event=started")
-
-	response = urllib2.urlopen(getRequest)
-	bencodedResponse = response.read()
-	decodedResponse = bencode.bdecode(bencodedResponse)
-	peerList = decodedResponse["peers"]
-	print type(peerList)
 
 	# getRequest = urllib.quote(getRequest)
 	# print getRequest
@@ -105,5 +54,67 @@ def main(args):
 	# print trackerSocket.recv(4096)
 
 	# trackerSocket.close()
+
+def getPeerList(metaDataPath):
+
+	decodedData = bencode.bdecode(open(metaDataPath, 'rb').read())
+
+	torrentTrackerUrl = decodedData["announce"]
+	slash = torrentTrackerUrl.find("/")
+	torrentTrackerHostName = torrentTrackerUrl[slash + 2:]
+	slash2 = torrentTrackerHostName.find("/")
+	torrentTrackerHostName = torrentTrackerHostName[:slash2]
+	colon = torrentTrackerHostName.find(":")
+	torrentTrackerHostName = torrentTrackerHostName[:colon]
+	# print torrentTrackerUrl
+	# print torrentTrackerHostName
+
+	sha1 = hashlib.sha1()
+	bencodedInfo = bencode.bencode(decodedData["info"])
+	sha1.update(bencodedInfo)
+	hashed_info = sha1.digest()
+
+	info_hash = urllib.quote(hashed_info)
+	peer_id = "abcdefghijklmonpqrst"
+	port = "6881"
+	uploaded = 0
+	downloaded = 0 
+	left = decodedData["info"]["length"]
+	compact = 0
+	no_peer_id = 0
+	event = "started"
+
+	# print(str(to"rrentTrackerHostName))
+	# print decodedDict["announce-list"]
+
+	getRequest = ( torrentTrackerUrl +
+	"?info_hash=" + info_hash  +
+	"&peer_id=" + peer_id  +
+	"&port=" + port +
+	"&uploaded=" + str(uploaded)  +
+	"&downloaded=" + str(downloaded)  +
+	"&left=" + str(left) +
+	"&compact=" + str(1) +
+	"&no_peer_id=0" +
+	"&event=started")
+
+	response = urllib2.urlopen(getRequest)
+	bencodedResponse = response.read()
+	decodedResponse = bencode.bdecode(bencodedResponse)
+	peerListString = decodedResponse["peers"]
+	peerList = []
+
+	for i in range(0, len(peerListString), 6):
+		ipAddress = (str(ord(peerListString[i:i+1])) + "."
+					+ str(ord(peerListString[i+1:i+2])) + "."
+					+ str(ord(peerListString[i+2:i+3])) + "."
+					+ str(ord(peerListString[i+3:i+4])))
+		portNumber = str((ord(peerListString[i+4:i+5]) << 8) |  ord(peerListString[i+5:i+6]))
+		peerList.append((ipAddress,portNumber))
+	
+	return peerList
+
+
+
 
 main(sys.argv)
